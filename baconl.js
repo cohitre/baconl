b6f26362d0c8f01c8d7d3270a5445c0521d8cab0
cohitre <carlosrr@gmail.com>
2008-11-30 14:50:24 -0800

    Adding node behavior with appending and prepending

--- baconl.js
+++ baconl.js
@@ -1,296 +1,179 @@
-var baconl = (function() {
-    function parseDefinition( template ) {    
-        if ( template === undefined ) { return undefined; }
-        var tokens = {
-            id: undefined,
-            classes:[],
-            tag: undefined,
-            innerHTML: "" , 
-            openTag: function() {
-                if ( tokens.tag === undefined ) { return ""; }
-                var result = "<" + tokens.tag;
-                if ( tokens.id !== undefined ) { result += " id='" + tokens.id + "'"; }
-                if ( tokens.classes.length > 0 ) 
-                { 
-                    result+=" class='" + tokens.classes.join(" ") + "'"; 
-                }
-                return result + ">";
-            } , 
-            closeTag: function() {
-                if ( tokens.tag === undefined ) { return ""; }
-                return "</" + tokens.tag + ">";
-            }
-        };
-
-        var tagDefinition   = /^\s*%([A-Za-z][A-Za-z0-9]*)/;
-        var idDefinition    = /^\#([A-Za-z][A-Za-z0-9:_\-]*)/;
-        var classDefinition = /^\.([A-Za-z][A-Za-z0-9:_\-]*)/;
-
-        // TODO: parse JSON and use it for the element attributes
-        // var jsonDefinition = 
-
-        var innerHTMLDefinition = /\s*\\?((.|[\n])+)/m;
-
-        // Retrieve HTML tag
-        var nextToken = template.match( tagDefinition );
-        if ( !!nextToken )
-        {
-            tokens.tag = nextToken[1];
-        }
-
-
-        template = template.replace( tagDefinition , "" );
-
-        // Retrieve element ID
-        nextToken = template.match( idDefinition );
-        tokens.id = !!nextToken ? nextToken[1] : undefined;
-
-        template = template.replace( idDefinition , "" );
-
-        // Retrieve classes
-        nextToken = template.match( classDefinition );
-
-        while( !!nextToken )
-        {
-            tokens.classes.push( nextToken[1] );
-            template = template.replace( classDefinition , "" );
-            nextToken = template.match( classDefinition );
-        }
-
-        if ( tokens.tag === undefined && ( tokens.id !== undefined || tokens.classes.length > 0 ) ) 
-        {
-            tokens.tag = "div";
-        }
-
-        // Retrieve inner content
-        nextToken = template.match( innerHTMLDefinition );
-        tokens.innerHTML = !!nextToken ? nextToken[1] : undefined;
-
-        return tokens;
-    }
-    
-    function map( array , callb ) {
-        var result = [];
-        for (var i=0; i < array.length; i++) {
-            result.push( callb( i , array[i] ) );
-        };
-        return result;
-    }
-        
-    function nodeToHTML( node ) {
-
-        var element = parseDefinition( node.definition );
-        var result = element.openTag();
-        
-        if ( element.innerHTML !== undefined )
-        {
-            result += element.innerHTML;
-        } 
-
-        result += map( node.childNodes , function(index , childNode ){
-            return nodeToHTML( childNode );
-        } ).join("\n");
-        
-        result += element.closeTag();
-        return result;
-    }
-    
+var baconl = (function() {    
     var self = function( template ) {
-        var tree = baconize( template.split("\n") );
-        return nodeToHTML( tree );
-    }
-    
-    function baconize( unreadBuffer , parentNode ) {
-        if ( parentNode === undefined )
-        {
-            parentNode = { 
-                depth: 0 , 
-                childNodes: [] ,
-                definition: ""
-            } ;
+        var tree;
+        if ( template.isNode !== true ) {
+            tree = baconl.baconize( template );
         }
-        function lineDepth( line ) {
-            return line.match(/^\s*/)[0].length/2 + 1;
+        else {
+            tree = template;
         }
-        function isChild( line ) {
-            return lineDepth(line) === parentNode.depth+1;
-        }
-
-        for ( var i = 0 ; i < unreadBuffer.length ; i++ ) {
-            if ( isChild( unreadBuffer[i] ) ) {
-                var child = baconize( unreadBuffer.slice( i+1 ) , {
-                    depth: parentNode.depth + 1 , 
-                    childNodes: [],
-                    definition: unreadBuffer[i]
-                });
-                parentNode.childNodes.push( child );
-            }
-            else if ( lineDepth(unreadBuffer[i]) <= parentNode.depth )
-            {
-                return parentNode;            
-            }
-        }
-        return parentNode;
+        return tree.toHTML();
     }
     
     return self    
 })();
 
+baconl.map = function map( array , callb ) {
+    var result = [];
+    for (var i=0; i < array.length; i++) {
+        result.push( callb( i , array[i] ) );
+    };
+    return result;
+}
 
-// 
-// function baconl1( template ) {
-//     var $ = jQuery;
-//     
-//     function toHTML( elements ) {
-//         var collectedElements = $.map( elements , function( element , index ) {            
-//             var parsedItems = baconl.parseTemplate( element.definition );
-// 
-//             function repeat( string , times ) {
-//                 var result = "";
-//                 for (var i=0; i < times; i++) {
-//                     result += string
-//                 };
-//                 return result;
-//             }
-// 
-//             if ( parsedItems.tag === undefined )
-//             {
-//                 return "\n" + repeat( "  " , element.depth) + parsedItems.innerHTML;
-//             }
-// 
-//             var self = $(parsedItems.openTag()+parsedItems.closeTag());
-// 
-//             if ( parsedItems.innerHTML !== undefined )
-//             {
-//                 self.html( parsedItems.innerHTML );
-//             }
-// 
-//             $.each( element.body , function( index, child) {
-//                 self.append(
-//                     baconl(child.definition) , 
-//                     toHTML( child.body )
-//                 )
-//             });
-//             
-//             return self;
-//         });
-//         
-//         var result;
-//         $.each( collectedElements , function( i , el ) {
-//             if ( result === undefined )
-//             {
-//                 result = el;
-//             }
-//             else
-//             {
-//                 result.add( el );
-//             }
-//         })
-//         return result;
-//     }
-//     return toHTML( baconl.splitElements(template) );
-// }
-// 
-// baconl.splitElements = function( template ) {
-//     var depth = 0;
-//     var lines = template.split( "\n" );
-// 
-//     function moveToDepth( lines , depth ) {
-//         while ( lines.length > 0 && lines[0].match( /^[ ]*/ )[0].length/2 > depth )
-//         {
-//             lines = lines.slice( 1 );
-//         }
-//         return lines;
-//     }
-//     
-//     function buildDepth( lines , depth ){
-//         var elements = [];
-// 
-//         while ( lines.length > 0 && lines[0].match( /^[ ]*/ )[0].length/2 === depth )
-//         {
-//             var el = {
-//                 definition: $.trim( lines[0] ) , 
-//                 body: buildDepth( lines.slice( 1 ) , depth+1 ) || [],
-//                 depth: depth
-//             };
-//             elements.push( el );
-//             
-//             lines = moveToDepth( lines.slice(1) , depth );
-//         }
-//         return elements;
-//     }    
-//     
-//     return buildDepth( lines , 0);
-// }
-// 
-// baconl.parseTemplate = function( template ) {    
-//     if ( template === undefined ) { return undefined; }
-//     var tokens = {
-//         id: undefined,
-//         classes:[],
-//         tag: undefined,
-//         innerHTML: "" , 
-//         openTag: function() {
-//             if ( tokens.tag === undefined ) { return ""; }
-//             var result = "<" + tokens.tag;
-//             if ( tokens.id !== undefined ) { result += " id='" + tokens.id + "'"; }
-//             if ( tokens.classes !== undefined ) 
-//             { 
-//                 result+=" class='" + tokens.classes.join(" ") + "'"; 
-//             }
-//             return result + ">";
-//         } , 
-//         closeTag: function() {
-//             if ( tokens.tag === undefined ) { return ""; }
-//             return "</" + tokens.tag + ">";
-//         }
-//         
-//     };
-// 
-//     var tagDefinition   = /^\s*%([A-Za-z][A-Za-z0-9]*)/;
-//     var idDefinition    = /^\#([A-Za-z][A-Za-z0-9:_\-]*)/;
-//     var classDefinition = /^\.([A-Za-z][A-Za-z0-9:_\-]*)/;
-// 
-//     // TODO: parse JSON and use it for the element attributes
-//     // var jsonDefinition = 
-// 
-//     var innerHTMLDefinition = /\s*\\?((.|[\n])+)/m;
-//     
-//     // Retrieve HTML tag
-//     var nextToken = template.match( tagDefinition );
-//     if ( !!nextToken )
-//     {
-//         tokens.tag = nextToken[1];
-//     }
-//     
-// 
-//     template = template.replace( tagDefinition , "" );
-// 
-//     // Retrieve element ID
-//     nextToken = template.match( idDefinition );
-//     tokens.id = !!nextToken ? nextToken[1] : undefined;
-//     
-//     template = template.replace( idDefinition , "" );
-//     
-//     // Retrieve classes
-//     nextToken = template.match( classDefinition );
-//     
-//     while( !!nextToken )
-//     {
-//         tokens.classes.push( nextToken[1] );
-//         template = template.replace( classDefinition , "" );
-//         nextToken = template.match( classDefinition );
-//     }
-//     
-//     if ( tokens.tag === undefined && ( tokens.id !== undefined || tokens.classes.length > 0 ) ) 
-//     {
-//         tokens.tag = "div";
-//     }
-//         
-//     // Retrieve inner content
-//     nextToken = template.match( innerHTMLDefinition );
-//     tokens.innerHTML = !!nextToken ? nextToken[1] : undefined;
-//     
-//     return tokens;
-// }
-// 
+
+baconl.parse = function( template ) {
+    if ( template === undefined ) { return undefined; }
+    var tokens = {
+        id: undefined,
+        classes:[],
+        tag: undefined,
+        innerHTML: "" , 
+        openTag: function() {
+            if ( tokens.tag === undefined ) { return ""; }
+            var result = "<" + tokens.tag;
+            if ( tokens.id !== undefined ) { result += " id='" + tokens.id + "'"; }
+            if ( tokens.classes.length > 0 ) { 
+                result+=" class='" + tokens.classes.join(" ") + "'"; 
+            }
+            return result + ">";
+        } , 
+        closeTag: function() {
+            if ( tokens.tag === undefined ) { return ""; }
+            return "</" + tokens.tag + ">";
+        }
+    };
+
+    var tagDefinition   = /^\s*%([A-Za-z][A-Za-z0-9]*)/;
+    var idDefinition    = /^\#([A-Za-z][A-Za-z0-9:_\-]*)/;
+    var classDefinition = /^\.([A-Za-z][A-Za-z0-9:_\-]*)/;
+
+    // TODO: parse JSON and use it for the element attributes
+    // var jsonDefinition = 
+
+    var innerHTMLDefinition = /\s*\\?((.|[\n])+)/m;
+
+    // Retrieve HTML tag
+    var nextToken = template.match( tagDefinition );
+    if ( !!nextToken ) {
+        tokens.tag = nextToken[1];
+    }
+
+
+    template = template.replace( tagDefinition , "" );
+
+    // Retrieve element ID
+    nextToken = template.match( idDefinition );
+    tokens.id = !!nextToken ? nextToken[1] : undefined;
+
+    template = template.replace( idDefinition , "" );
+
+    // Retrieve classes
+    nextToken = template.match( classDefinition );
+
+    while( !!nextToken ) {
+        tokens.classes.push( nextToken[1] );
+        template = template.replace( classDefinition , "" );
+        nextToken = template.match( classDefinition );
+    }
+
+    if ( tokens.tag === undefined && ( tokens.id !== undefined || tokens.classes.length > 0 ) ) {
+        tokens.tag = "div";
+    }
+
+    // Retrieve inner content
+    nextToken = template.match( innerHTMLDefinition );
+    tokens.innerHTML = !!nextToken ? nextToken[1] : undefined;
+
+    return tokens;
+
+}
+
+baconl.node = function( definition ) {
+    if ( definition !== undefined && definition.isNode ) {
+        return definition;
+    }
+    var self = {
+        definition: definition||"" , 
+        isNode: true, 
+        depth: 0 , 
+        childNodes: [] , 
+        parentNode: undefined, 
+        parent: function() { return self.parentNode; }
+    };
+    
+    function makeChild( child ) {
+        child = baconl.node( child );
+        child.depth = self.depth + 1;
+        child.parentNode = self;
+        return child;
+    }
+
+    self.prepend = function() {
+        for ( var i = 0 ; i < arguments.length ; i++ )
+        {
+            self.childNodes.splice(0,0, makeChild( arguments[i] ) );
+        }
+        return self;        
+    }
+    
+    self.append = function() {
+        for ( var i = 0 ; i < arguments.length ; i++ )
+        {
+            self.childNodes.push( makeChild( arguments[i] ) );
+        }
+        return self;
+    }
+    
+    
+    
+    self.toHTML = function() {
+        var node = self;
+        var element = baconl.parse( node.definition );
+        var result = element.openTag();
+
+        if ( element.innerHTML !== undefined ) {
+            result += element.innerHTML;
+        } 
+
+        result += baconl.map( node.childNodes , function(index , childNode ){
+            return childNode.toHTML();
+        } ).join("\n");
+
+        result += element.closeTag();
+        return result;        
+    }
+    
+    return self;
+}
+
+baconl.baconize = function( unreadBuffer , parentNode ) {
+    if ( unreadBuffer.constructor === String ) {
+        unreadBuffer = unreadBuffer.split("\n");
+    }
+    
+    if ( parentNode === undefined ) {
+        parentNode = baconl.node();
+    }
+    function lineDepth( line ) {
+        return line.match(/^\s*/)[0].length/2 + 1;
+    }
+    function isChild( line ) {
+        return lineDepth(line) === parentNode.depth+1;
+    }
+
+    for ( var i = 0 ; i < unreadBuffer.length ; i++ ) {
+        if ( isChild( unreadBuffer[i] ) ) {
+            var child = baconl.node( unreadBuffer[i] ) ;
+            parentNode.append( child );
+            baconl.baconize( 
+                unreadBuffer.slice( i+1 ) , 
+                child 
+            );
+        }
+        else if ( lineDepth(unreadBuffer[i]) <= parentNode.depth ) {
+            return parentNode;            
+        }
+    }
+    return parentNode;
+}
--- tests.rb
+++ tests.rb
@@ -11,9 +11,13 @@ class BaconlTest < Test::Unit::TestCase
   def eval( val )
     @context.evaluate( val )
   end
-  
+
   def baconl( val )
     eval( "baconl(\"#{val}\");" )
+  end
+
+  def node( definition )
+    eval( "baconl.node(\"#{definition}\"); ")
   end
 
   def test_simple_strings
@@ -21,16 +25,52 @@ class BaconlTest < Test::Unit::TestCase
     assert_equal( "welcome\nmr. pope"  , baconl('welcome\nmr. pope') )
     assert_equal( "welcome\nmr.\npope" , baconl('welcome\nmr.\npope'))
   end
-  
+
   def test_markup
     assert_equal("<h1>The cat in the hat</h1>" ,  baconl('%h1 The cat in the hat') )
     assert_equal("<h1>The cat in the hat</h1>\n<h2>by <span>Dr. Seuss</span></h2>" ,  baconl('%h1 The cat in the hat\n%h2 by \n  %span Dr. Seuss') )
   end
-  
+
   def test_attributes
     assert_equal( "<h1 class='header'>The itinerant earthworm</h1>" , baconl("%h1.header The itinerant earthworm") )
-    assert_equal( "<h1 id='best-header' class='header'>The itinerant earthworm</h1>" , baconl("%h1#best-header.header The itinerant earthworm") )    
-    assert_equal( "<h1 id='best-header' class='header'>The itinerant earthworm</h1>\n<h2 id='second-best-header'>I wish i was best</h2>" , baconl("%h1#best-header.header The itinerant earthworm\\n%h2#second-best-header I wish i was best") )    
+    assert_equal( "<h1 id='best-header' class='header'>The itinerant earthworm</h1>" , baconl("%h1#best-header.header The itinerant earthworm") )
+    assert_equal( "<h1 id='best-header' class='header'>The itinerant earthworm</h1>\n<h2 id='second-best-header'>I wish i was best</h2>" , baconl("%h1#best-header.header The itinerant earthworm\\n%h2#second-best-header I wish i was best") )
+  end
+
+  def test_construct_node
+    assert_equal( "<div></div>" , eval('baconl.node( "%div" ).toHTML();') )
+    assert_equal( "<div></div>" , eval('baconl.node(baconl.node( "%div" )).toHTML();') )    
+    assert_equal( "<div>body</div>" , eval('baconl.node( "%div body" ).toHTML();') )
+    assert_equal( "<div class='home'>body</div>" , eval('baconl.node( "%div.home body" ).toHTML();') )    
+  end
+    
+  def test_prepend_node
+    doc = eval(%Q[
+      baconl.node( "%div" )
+      .prepend( baconl.node("%h1 Welcome!") )
+    .toHTML();])
+    assert_equal( "<div><h1>Welcome!</h1></div>" , doc )    
+    doc = eval(%Q[
+      baconl.node( "%div" )
+      .prepend( baconl.node("%h1 Second") )
+      .prepend( baconl.node("%h2 first") )      
+    .toHTML();])
+    assert_equal( "<div><h2>first</h2>\n<h1>Second</h1></div>" , doc )    
+  end
+
+  def test_append_node
+    doc = eval(%Q[
+      baconl.node( "%div" )
+      .append( baconl.node("%h1 Welcome!") )
+    .toHTML();])
+    assert_equal( "<div><h1>Welcome!</h1></div>" , doc )
+  end
+
+  def test_append_template
+    doc = eval(%Q[
+      baconl.node( "%div" ).append( "%h1 Welcome!" ).toHTML();
+    ])
+    assert_equal( "<div><h1>Welcome!</h1></div>" , doc )
   end
 
 end
